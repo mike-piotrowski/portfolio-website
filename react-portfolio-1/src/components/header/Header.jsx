134a89aa8f9a35a3a562f13a4dc1708d6d00ab21
piotro1994@gmail.com <piotro1994@gmail.com>
2022-06-21 17:22:13 +0200

    Added Header & mk responsive

--- react-portfolio-1/src/components/header/Header.jsx
+++ react-portfolio-1/src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './header.css';
 import CTA from './CTA';
+import ME from '../../assets/me.png'
+import HeaderSocial from "./HeaderSocials";
 
 const Header = () => {
   return (
@@ -10,6 +12,11 @@ const Header = () => {
         <h1>Michał Piotrowski</h1>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
+        <HeaderSocial />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>
   )
