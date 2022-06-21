55b45ca89b7b7dddf496bf76b6ee672aa868637f
piotro1994@gmail.com <piotro1994@gmail.com>
2022-06-21 17:24:19 +0200

    Added Header Section & mk responsive

--- react-portfolio-1/src/components/header/Header.jsx
+++ react-portfolio-1/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import './header.css';
-import CTA from './CTA';
-import ME from '../../assets/me.png'
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
 import HeaderSocial from "./HeaderSocials";
 
 const Header = () => {
@@ -16,10 +16,12 @@ const Header = () => {
         <div className="me">
           <img src={ME} alt="me" />
         </div>
-        <a href="#contact" className="scroll__down">Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
-  )
+  );
 };
 
 export default Header;
--- react-portfolio-1/src/components/header/HeaderSocials.jsx
+++ react-portfolio-1/src/components/header/HeaderSocials.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { BsLinkedin } from 'react-icons/bs'
-import { FaGithub } from 'react-icons/fa'
-import { SiNetlify } from 'react-icons/si'
-
-
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { SiNetlify } from "react-icons/si";
 
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
-        <a href='https://linkedin.com' target="_blank"><BsLinkedin /></a>
-        <a href='https://github.com' target="_blank"><FaGithub /></a>
-        <a href='https://app.netlify.com' target="_blank"><SiNetlify /></a>
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://app.netlify.com" target="_blank">
+        <SiNetlify />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
