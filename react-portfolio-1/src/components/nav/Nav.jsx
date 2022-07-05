37cb4416533f59620e593f63cedc6667ded7b54b
piotro1994@gmail.com <piotro1994@gmail.com>
2022-07-05 17:06:59 +0200

    Added portfolio component

--- react-portfolio-1/src/components/nav/Nav.jsx
+++ react-portfolio-1/src/components/nav/Nav.jsx
@@ -3,7 +3,6 @@ import "./nav.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
-import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDetail } from "react-icons/bi";
 import { useState } from "react";
 
@@ -28,13 +27,7 @@ const Nav = () => {
       >
         <BiBook />
       </a>
-      <a
-        href="#services"
-        onClick={() => setActiveNav("#services")}
-        className={activeNav === "#services" ? "active" : ""}
-      >
-        <RiServiceLine />
-      </a>
+
       <a
         href="#contact"
         onClick={() => setActiveNav("#contact")}
--- react-portfolio-1/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-1/src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,62 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/movieee.png";
+import IMG2 from "../../assets/skapp.png";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>Movies Streaming Hub</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/mike-piotrowski/netflix_clone"
+              className="btn"
+              target="_blank"
+            >
+              Github
+            </a>
+            <a
+              href="https://gracious-almeida-358b14.netlify.app/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>Food Recipies Website</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/mike-piotrowski/recipies-web"
+              className="btn"
+              target="_blank"
+            >
+              Github
+            </a>
+            <a
+              href="https://spontaneous-tulumba-c4687a.netlify.app/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
